8d4266882b9f54d11bf88776918be89e7239b2dc
johannes karoff <johannes@karoff.net>
2020-10-04 17:32:31 +0200

    WhatsWrong2

--- app/WhatsWrong2.scala
+++ app/WhatsWrong2.scala
@@ -37,4 +37,23 @@ object WhatsWrong2 {
       (ceo, enterprise)
     }
   }
+
+  // There are multiple problems with this code:
+  // 1) Calling `get` on an option is not recommended as it will fail with a
+  //    runtime error, we should handle the case where `ceo_id` is `None` - or
+  //    even better we could change the parameter to be `ceo_id: String` and
+  //    map over the `Option` on the call side.
+  // 2) Doing a for comprehension with futures like above, will make the two
+  //    futures run in succession, we should run the two futures in parallel,
+  //    as they do not depend on each other. Also using the `zip` method on
+  //    futures is more concise in this case.
+  // 3) The EnterpriseDao is wrong, because Mark Zuckerberg is the CEO of
+  //    Facebook and Sundar Pichai is the CEO of google (not the other way
+  //    around).
+  def getCEOAndEnterpriseBetter(ceo_id: Option[String]): Future[(Option[CEO], Option[Enterprise])] =
+    ceo_id.fold(
+      Future.successful((Option.empty[CEO], Option.empty[Enterprise]))
+    ) { ceo_id =>
+      CEODao.byId(ceo_id) zip EnterpriseDao.byCEOId(ceo_id)
+    }
 }
